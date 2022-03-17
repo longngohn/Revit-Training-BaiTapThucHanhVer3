b938ecdbe56224bb4619bbdb7b610832ac7a4cf3
Cool Cool <ngohailong1991@gmail.com>
2022-03-17 11:17:09 +0700

    update

---
+++ Lesson 9/9.3_WallFromAutoCAD/WallData.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Windows.Forms;
+
+namespace AlphaBIM
+{
+    public class WallData
+    {
+        public WallData(PlanarFace planarFace)
+        {
+
+            try
+            {
+                EdgeArrayArray edgeLoops = planarFace.EdgeLoops;
+
+                foreach (EdgeArray edgeArray in edgeLoops)
+                {
+                    foreach (Edge edge in edgeArray)
+                    {
+                        Curve newCur = edge.AsCurve();
+                        AllCurve.Append(newCur);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+        }
+        /// <summary>
+        /// Đường bao của Hatch sàn
+        /// </summary>
+        public CurveArray AllCurve { get; set; } = new CurveArray();
+    }
+}
